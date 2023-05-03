733de70762f1c5e42dddf95dcffa63b2c0e8a912
Sensornyy <maksym.koriak@chisw.com>
2023-05-03 22:51:52 +0300

    feat: add dependency injection

---
+++ lib/dependencies.dart
@@ -0,0 +1,40 @@
+import 'package:get_it/get_it.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import 'package:cafe_app/data/repository/repository_impl.dart';
+import 'package:cafe_app/data/sources/local_data_source.dart';
+import 'package:cafe_app/domain/repository/repository.dart';
+import 'package:cafe_app/domain/use_cases/get_news_use_case.dart';
+import 'package:cafe_app/domain/use_cases/get_user_use_case.dart';
+
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  sl.registerLazySingleton(() => GetNewsUseCase(sl()));
+
+  sl.registerLazySingleton(() => GetUserUseCase(sl()));
+
+  sl.registerLazySingleton<Repository>(
+    () => RepositoryImpl(sl()),
+  );
+
+  sl.registerLazySingleton<LocalDataSource>(
+    () => LocalDataSourceImpl(sl()),
+  );
+
+  //TODO: create 2 databases for user and news, use the code below as an example
+  //TODO: add mock data to databases
+
+  // var databasesPath = await getDatabasesPath();
+  // String path = join(databasesPath, 'cafe_app.db');
+  //
+  // Database database = await openDatabase(path, version: 1,
+  //     onCreate: (Database db, int version) async {
+  //   await db.execute(
+  //       'CREATE TABLE recipes (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, calories DOUBLE, image TEXT, ingredientLines TEXT)');
+  // });
+  //
+  // sl.registerSingletonAsync<Database>(() async => database);
+}
